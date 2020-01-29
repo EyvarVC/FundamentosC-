0245b54ebe61f4ec0044de86d0d267741788c984
Eyvar Velázquez <eyvarvelazquez@gmail.com>
2020-01-29 10:15:06 -0600

    Optimizando la inicialización del arreglo
    En branch Pruebas

--- Program.cs
+++ Program.cs
@@ -12,30 +12,12 @@ namespace Etapa1
             escuela.Ciudad = "Bogotá";
             escuela.TipoEscuela = TiposEscuela.Primaria;
 
-            var arregloCursos = new Curso[3];
-
-            arregloCursos[0] = new Curso()
+            Curso[] arregloCursos =
             {
-                Nombre = "101",
-                Jornada = TiposJornada.Noche
+                new Curso() {Nombre = "101", Jornada = TiposJornada.Noche},
+                new Curso() {Nombre = "201", Jornada = TiposJornada.Tarde},
+                new Curso() {Nombre = "301", Jornada = TiposJornada.Tarde}
             };
-
-            var curso2 = new Curso()
-            {
-                Nombre = "201",
-                Jornada = TiposJornada.Tarde
-            };
-
-            arregloCursos[1] = curso2;
-
-            arregloCursos[2] = new Curso
-            {
-                Nombre = "301",
-                Jornada = TiposJornada.Tarde
-            };
-
-
-
 
             Console.WriteLine(escuela);
             System.Console.WriteLine("===============");
@@ -50,45 +32,45 @@ namespace Etapa1
 
         }
 
-            private static void ImprimirCursosWhile(Curso[] ListaCursos)
+        private static void ImprimirCursosWhile(Curso[] ListaCursos)
+        {
+            int contador = 0;
+            while (contador < ListaCursos.Length)
             {
-                int contador = 0;
-                while (contador<ListaCursos.Length)
-                {
-                    Console.WriteLine($"Curso:{ListaCursos[contador].Nombre} ID:{ListaCursos[contador].UniqueId}");
-                    contador++;
-                }
+                Console.WriteLine($"Curso:{ListaCursos[contador].Nombre} ID:{ListaCursos[contador].UniqueId}");
+                contador++;
             }
+        }
 
-            private static void ImprimirCursosDoWhile(Curso[] ListaCursos)
+        private static void ImprimirCursosDoWhile(Curso[] ListaCursos)
+        {
+            int contador = 0;
+
+            do
             {
-                int contador = 0;
+                Console.WriteLine($"Curso:{ListaCursos[contador].Nombre} ID:{ListaCursos[contador].UniqueId}");
+                contador++;
+            } while (contador < ListaCursos.Length);
 
-                do
-                {
-                    Console.WriteLine($"Curso:{ListaCursos[contador].Nombre} ID:{ListaCursos[contador].UniqueId}");
-                    contador++;
-                } while (contador<ListaCursos.Length);
-                
 
-                
-            }
 
-             private static void ImprimirCursosFor(Curso[] ListaCursos)
+        }
+
+        private static void ImprimirCursosFor(Curso[] ListaCursos)
+        {
+            for (int i = 0; i < ListaCursos.Length; i++)
             {
-                for(int i=0; i<ListaCursos.Length; i++)
-                {
-                    Console.WriteLine($"Curso:{ListaCursos[i].Nombre} ID:{ListaCursos[i].UniqueId}");
-                } 
+                Console.WriteLine($"Curso:{ListaCursos[i].Nombre} ID:{ListaCursos[i].UniqueId}");
             }
+        }
 
-private static void ImprimirCursosForEach(Curso[] ListaCursos)
+        private static void ImprimirCursosForEach(Curso[] ListaCursos)
+        {
+            foreach (Curso item in ListaCursos)
             {
-                foreach (Curso item in ListaCursos)
-                {
-                    Console.WriteLine($"Curso:{item.Nombre} ID:{item.UniqueId}");
-                }
+                Console.WriteLine($"Curso:{item.Nombre} ID:{item.UniqueId}");
             }
+        }
 
 
 
